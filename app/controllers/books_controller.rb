f69005652906038a366c3e36bc53bb01b5fe364b
EC2 Default User <ec2-user@ip-172-31-19-26.ap-northeast-1.compute.internal>
2023-08-09 19:50:15 +0000

    valid

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,25 +1,43 @@
 class BooksController < ApplicationController
   def index
     @Books = Book.all
-    
+    @book = Book.new
+
   end
-  
+
   def create
     @book = Book.new(book_params)
-    @book.save
+    if @book.save
     redirect_to book_path(@book)
+    flash[:notice] = "Book was successfully created."
+    else
+    @books = Book.all
+    render :index
+    end
   end
 
   def show
     @book = Book.find(params[:id])
+    flash[:notice] = "Book was successfully updated."
   end
 
   def edit
+    @book = Book.find(params[:id])
+
   end
-  
-  def destory
+
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to book_path(@book)
   end
-  
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+
   private
   def book_params
     params.require(:book).permit(:title, :body)
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
   resources :books
   
   post 'books' => 'book#create'
-
+  
 end
